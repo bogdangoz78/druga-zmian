1a98cd32d589b49274872d0f192d889c6d815f72
mmigryt1@wp.pl <mmigryt1@wp.pl>
2017-10-20 12:41:42 +0200

    mnożenie

--- program2.c
+++ program2.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
 int mnoz(int a, int b) {
-         return a * b;
+
+	return a * b;
+
 }
-int dodaj (int a, int b){return a + b;
+int dodaj (int a, int b){
+
+	return a + b;
 }
 
 
 int main(void)
-
-
 {
-
-
 	int a= 2, b = 5;    /*deklaracja wraz z inicjalizacja*/
-	char wybor = '+';
+	char wybor;
+
+	scanf("%c" , &wybor); /*czytaj z konsoli i zapisz wartosc pod wybor */
+
 	/*if (wyrażenie) {blokjeśli wyrażenie prawdziwe}
 	      else {blok jesli wydarzenie fałszywa}; */
-		if (wybor == '*'){
-	printf("wynik mnozenia: %i\n", mnoz(a, b));
+	if (wybor == '*'){
+		printf("wynik mnozenia: %i\n", mnoz(a, b));
 	}else{
-	printf("wynik dodawania: %i\n", dodaj(a, b));
-}; /* ten srednik jest opcjonalny jesli wystepuje klamra*/
-
-   
+		printf("wynik dodawania: %i\n", dodaj(a, b));
+	}; /* ten srednik jest opcjonalny jesli wystepuje klamra*/
 
 	return 0;
-
 }
-
