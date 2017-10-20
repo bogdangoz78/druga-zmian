c67beb7cfb8293e8464019fd35ce0ede7c352240
mmigryt1@wp.pl <mmigryt1@wp.pl>
2017-10-20 09:40:40 +0200

    implementacja pierwszych programów w jezyku C

---
+++ program1.c
@@ -0,0 +1,22 @@
+#include <stdio.h> /* biblioteka standardowa input-output */
+
+/* y = f(x) */
+int main() /*funkcja glowna programu */
+/* typ zwracanej wartosci (int-wartosc calkowita */
+{
+puts ("pierwszy program");      /* puts- funkcja z biblioteki standardowej */
+                                /*puts - put string*/
+				/* wyswietlajaca ciag znaków */
+				/*automatycznie p[rzechoidzi do nowej lini*/
+
+putchair('g');
+putchar('\n'); /*znak konca linii*/
+putchar('/t'); /*tabulator*/
+putchar('/r'); /*powrot karetki - kursor wraca na poczatek linii*/
+putchar('\\'); /*wypisuje \ */
+
+		return 0;
+
+} /* klamry oznaczaja blok programu, zwykle cialo funkcji */
+
+
