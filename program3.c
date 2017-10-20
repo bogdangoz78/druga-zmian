6aeffdb4b016fb00833dfc1cf178e97a9c168bbe
mmigryt1@wp.pl <mmigryt1@wp.pl>
2017-10-20 13:36:22 +0200

    kalkulator

---
+++ program3.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+int mnoz(int a, intB){
+	printf("wynik mnozenia: %i\n",a * b0:
+	return a * b;}
+int dodaj(int a, int B){
+	printf("wynik dodawania: %i\n",a /b);
+	return a + b;
+}
+int odejmij (int a, int b){
+	printf("wynik odejmowania: %i\n",a - b);
+	return a - b
+int dziel(int a, int b){
+printf("wynik dzielenia: %i\n",a / b);
+return a / b
+
+int main(void)
+char wybor;
+scanf("%c" &wybor); /*czytaj z konsoli i zapisz wartosc pod wybor*/
+
+if(wybor
